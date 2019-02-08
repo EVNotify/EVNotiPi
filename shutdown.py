e42bbdccb4c296c32490702b488e2c3737b4715d
snaptec <36848507+snaptec@users.noreply.github.com>
2019-02-08 10:17:23 +0100

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -1,3 +1,9 @@
+# This file needs to be start with root rights or as user pi with sudo at boot
+#
+#The script listens to GPIO 22 wheter the attached wemos pulls up D5 for shutting down the Rpi
+#
+# ToDo: atreboot handler should be written
+
 import RPi.GPIO as GPIO
 import time
 
