0596286824a927ecee6d000eeccfb74471033aea
snaptec <36848507+snaptec@users.noreply.github.com>
2019-03-04 15:07:16 +0100

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -1,15 +1,14 @@
 # This file needs to be start with root rights or as user pi with sudo at boot
 #
-#The script listens to GPIO 22 wheter the attached wemos pulls up D5 for shutting down the Rpi
+#The script listens to GPIO 24 wheter the attached wemos pulls up D5 for shutting down the Rpi
 #
-# ToDo: atreboot handler should be written
 
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(24, GPIO.IN)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True: 
     if (GPIO.input(24) == True): 
         from subprocess import call
