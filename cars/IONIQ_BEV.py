94a787133015b73e48c46961a5bdb4e4e0b53d55
Malte Schröder <norad@tnxip.de>
2019-03-04 12:44:13 +0100

    some reformatting

--- cars/IONIQ_BEV.py
+++ cars/IONIQ_BEV.py
@@ -17,9 +17,11 @@ class IONIQ_BEV:
 
         chargingBits = raw[2101][0x7EC21][5] \
                 if 0x7EC21 in raw[2101] else None
-        dcBatteryCurrent = int.from_bytes(raw[2101][0x7EC21][6:7] + raw[2101][0x7EC22][0:1], byteorder='big', signed=True) / 10.0 \
+        dcBatteryCurrent = int.from_bytes(raw[2101][0x7EC21][6:7] + raw[2101][0x7EC22][0:1],
+                byteorder='big', signed=True) / 10.0 \
                 if 0x7EC21 in raw[2101] and 0x7EC22 in raw[2101] else None
-        dcBatteryVoltage = int.from_bytes(raw[2101][0x7EC22][1:3], byteorder='big', signed=False) / 10.0 \
+        dcBatteryVoltage = int.from_bytes(raw[2101][0x7EC22][1:3],
+                byteorder='big', signed=False) / 10.0 \
                 if 0x7EC22 in raw[2101] else None
 
         data = {'SOC_BMS':      raw[2101][0x7EC21][0] / 2.0 \
@@ -29,23 +31,37 @@ class IONIQ_BEV:
                 'EXTENDED': {
                     'auxBatteryVoltage':        raw[2101][0x7EC24][4] / 10.0 \
                         if 0x7EC24 in raw[2105] else None,
-                    'batteryInletTemperature':  int.from_bytes(raw[2101][0x7EC23][2:3], byteorder='big', signed=True) \
+
+                    'batteryInletTemperature':  int.from_bytes(raw[2101][0x7EC23][2:3],
+                        byteorder='big', signed=True) \
                         if 0x7EC23 in raw[2105] else None,
-                    'batteryMaxTemperature':    int.from_bytes(raw[2101][0x7EC22][3:4], byteorder='big', signed=True) \
+
+                    'batteryMaxTemperature':    int.from_bytes(raw[2101][0x7EC22][3:4],
+                        byteorder='big', signed=True) \
                         if 0x7EC22 in raw[2105] else None,
-                    'batteryMinTemperature':    int.from_bytes(raw[2101][0x7EC22][4:5], byteorder='big', signed=True) \
+
+                    'batteryMinTemperature':    int.from_bytes(raw[2101][0x7EC22][4:5],
+                        byteorder='big', signed=True) \
                         if 0x7EC22 in raw[2105] else None,
+
                     'charging':                 1 if chargingBits != None and \
                             chargingBits & 0x80 == 0x80 else 0,
+
                     'normalChargePort':         1 if chargingBits != None and \
                             chargingBits & 0x20 == 0x20 else 0,
+
                     'rapidChargePort':          1 if chargingBits != None and \
                             chargingBits & 0x40 == 0x40 else 0,
+
                     'dcBatteryCurrent':         dcBatteryCurrent,
+
                     'dcBatteryPower':           dcBatteryCurrent * dcBatteryVoltage / 1000.0 \
                         if dcBatteryCurrent!= None and dcBatteryVoltage != None else None,
+
                     'dcBatteryVoltage':         dcBatteryVoltage,
-                    'soh':                      int.from_bytes(raw[2105][0x7EC24][0:2], byteorder='big', signed=False) / 10.0 \
+
+                    'soh':                      int.from_bytes(raw[2105][0x7EC24][0:2],
+                        byteorder='big', signed=False) / 10.0 \
                         if 0x7EC24 in raw[2105] else None,
                     }
                 }
