7d4f1cda9594d7e717abe0885bc6c4466aa1aef9
snaptec <36848507+snaptec@users.noreply.github.com>
2019-03-04 13:27:17 +0100

    Update shutdown.py
    GPIO 22 blocked by OBD HAT.
    Changed to 24 and changed reading for better stability

--- shutdown.py
+++ shutdown.py
@@ -9,13 +9,9 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-try:
-    while True:
-        button1_state = GPIO.input(22)
-        time.sleep(1)
-        if button1_state == False:
-            from subprocess import call
-            call("sudo shutdown -h now", shell=True)
-except:
-    GPIO.cleanup()
+GPIO.setup(24, GPIO.IN)
+while True: 
+    if (GPIO.input(24) == True): 
+        from subprocess import call
+        call("sudo shutdown -h now", shell=True)
+    time.sleep(1);
