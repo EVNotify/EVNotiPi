9ac61c544d1fe1b63afe00c9a4c3ad4edfda183c
Malte Schröder <malte@tnxip.de>
2019-03-19 22:03:14 +0100

    make shutdown.py work

--- shutdown.py
+++ shutdown.py
@@ -1,22 +1,18 @@
-# This file needs to be start with root rights or as user pi with sudo at boot
-#
-#The script listens to GPIO 22 wheter the attached wemos pulls up D5 for shutting down the Rpi
-#
-# ToDo: atreboot handler should be written
+#!/usr/bin/env python3
 
 import RPi.GPIO as GPIO
-import time
 from subprocess import call
 
-PIN_12VOK = 24
+PIN_12VOK = 20
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(PIN_12VOK, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 try:
     while True:
-        button1_state = GPIO.wait_for_edge(PIN_12VOK, GPIO.RISING)
+        button1_state = GPIO.wait_for_edge(PIN_12VOK, GPIO.RISING, timeout=1000)
         if button1_state == False:
-            call("sudo shutdown -h now", shell=True)
+            print("12V failing! Shutdown!")
+            call(['/bin/systemctl','poweroff'])
 finally:
     GPIO.cleanup()
