ad67be7ce61144cf2081ab20fc93470ecf770c45
snaptec <36848507+snaptec@users.noreply.github.com>
2019-04-13 14:51:01 +0200

    Update update.sh

--- runs/update.sh
+++ runs/update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd /var/www/html/PlugAndPlay
-cp /var/www/html/PlugAndPlay/config.json /tmp/config.json
+sudo cp /var/www/html/PlugAndPlay/config.json /tmp/config.json
 sudo git fetch origin
 sudo git reset --hard origin/master
-cp /tmp/config.json /var/www/html/PlugAndPlay/config.json 
-chmod 777 /var/www/html/PlugAndPlay/config.json 
-chmod 777 /var/www/html/PlugAndPlay/runs/update.sh
-chmod +x /var/www/html/PlugAndPlay/runs/*.sh
+sudo cp /tmp/config.json /var/www/html/PlugAndPlay/config.json 
+sudo chmod 777 /var/www/html/PlugAndPlay/config.json 
+sudo chmod 777 /var/www/html/PlugAndPlay/runs/update.sh
+sudo chmod +x /var/www/html/PlugAndPlay/runs/*.sh
 uuid=$(</sys/class/net/eth0/address)
-curl -d "update="PlugAndPlay$uuid"" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://openwb.de/tools/update.php
-cp web/update.html ./
-cp web/update.php ./
+curl -s -d "update="PlugAndPlay$uuid"" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://openwb.de/tools/update.php
+sudo cp web/update.html ./
+sudo cp web/update.php ./
