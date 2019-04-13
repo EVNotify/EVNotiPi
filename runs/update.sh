727e044d81854dcc5533b0603f44b0bff05359b4
snaptec <36848507+snaptec@users.noreply.github.com>
2019-04-13 14:33:01 +0200

    Update update.sh

--- runs/update.sh
+++ runs/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp /var/www/html/PlugAndPlay/config.json /tmp/config.json
 sudo git fetch origin
-sudo git fetch --hard origin/master
+sudo git reset --hard origin/master
 cp /tmp/config.json /var/www/html/PlugAndPlay/config.json 
 chmod 777 /var/www/html/PlugAndPlay/config.json 
 
