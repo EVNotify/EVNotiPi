2e9ac5580263e180350def5420cda0709377f3a2
snaptec <36848507+snaptec@users.noreply.github.com>
2019-04-13 14:31:07 +0200

    Create update.php

---
+++ web/update.php
@@ -0,0 +1,9 @@
+
+<?php
+
+        echo "Update wird durchgeführt, bitte nicht vom OBD Adapter trennen";
+        exec("/var/www/html/EVNotiPi/runs/update.sh");
+?>
+        <script type="text/javascript">
+            setTimeout(function() { window.location = "./index.html"; }, 30000);
+        </script>
