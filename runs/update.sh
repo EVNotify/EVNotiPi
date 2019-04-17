0d032173759bb285a7367d3337f0ed4de52eb552
snaptec <36848507+snaptec@users.noreply.github.com>
2019-04-17 14:47:07 +0200

    Update update.sh

--- runs/update.sh
+++ runs/update.sh
@@ -11,3 +11,4 @@ uuid=$(</sys/class/net/eth0/address)
 curl -s -d "update="PlugAndPlay$uuid"" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://openwb.de/tools/update.php
 sudo cp web/update.html ../
 sudo cp web/update.php ../
+sudo cp web/indexmain.html ../index.html
