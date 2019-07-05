c8acd700c7fa7bdb28feb04e98603e2773f2783a
Malte Schröder <malte@tnxip.de>
2019-07-05 18:02:56 +0200

    sync GPIO setup to PnP

--- shutdown.py
+++ shutdown.py
@@ -4,18 +4,17 @@ import RPi.GPIO as GPIO
 from subprocess import call
 from time import sleep
 
-PIN_12VOK = 20
+PIN_12VOK = 24
 MAX_FAIL = 10
 fail_cnt = 0
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(PIN_12VOK, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PIN_12VOK, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 try:
     while True:
-        #button1_state = GPIO.wait_for_edge(PIN_12VOK, GPIO.RISING, timeout=1000)
         button1_state = GPIO.input(PIN_12VOK)
-        #print(button1_state,fail_cnt)
+
         if button1_state == 1 and fail_cnt < MAX_FAIL :
             fail_cnt += 1
         elif fail_cnt > 0:
