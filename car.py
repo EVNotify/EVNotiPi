638a864749eb5d7cb2cf416218febc77b9f0151b
Malte Schröder <malte@tnxip.de>
2019-09-17 14:33:58 +0200

    implement polling loop for cars

---
+++ car.py
@@ -0,0 +1,94 @@
+from time import time
+from threading import Timer, Lock
+from readerwriterlock import rwlock
+from dongle import NoData, CanError
+import logging
+
+def ifbu(in_bytes): return int.from_bytes(in_bytes, byteorder='big', signed=False)
+def ifbs(in_bytes): return int.from_bytes(in_bytes, byteorder='big', signed=True)
+def ffbu(in_bytes): return float(int.from_bytes(in_bytes, byteorder='big', signed=False))
+def ffbs(in_bytes): return float(int.from_bytes(in_bytes, byteorder='big', signed=True))
+
+class DataError(Exception): pass
+
+class Car:
+    def __init__(self, config, dongle):
+        self.log = logging.getLogger("EVNotiPi/Car")
+        self.config = config
+        self.dongle = dongle
+        self.poll_interval = config['interval']
+        self.timer = None
+        self.data = {}
+        self.datalock = rwlock.RWLockWrite()
+        self.skip_polling = False
+        self.running = False
+        self.last_data = 0
+        self.watchdog = time()
+        self.watchdog_timeout = self.poll_interval * 10
+
+    def start(self):
+        self.running = True
+        self.timer = Timer(0, self.pollData)
+        self.timer.start()
+
+    def stop(self):
+        if self.running:
+            self.timer.cancel()
+        self.running = False
+
+    def pollData(self):
+        if not self.running: return
+
+        now = time()
+        self.watchdog = now
+
+        with self.datalock.gen_wlock():
+            if not self.skip_polling or self.dongle.isCarAvailable():
+                if self.skip_polling:
+                    self.log.info("Resume polling.")
+                    self.skip_polling = False
+                try:
+                    self.data = self.readDongle()
+                    self.last_data = now
+                except CanError as e:
+                    self.log.warning(e)
+                except NoData:
+                    self.log.info("NO DATA")
+                    if not self.dongle.isCarAvailable():
+                        self.log.info("Car off detected. Stop polling until car on.")
+                        self.skip_polling = True
+            else:
+                self.data = {}
+
+            if self.dongle.watchdog:
+                thresholds = self.dongle.watchdog.getThresholds()
+                if not 'ADDITIONAL' in self.data:
+                    self.data['ADDITIONAL'] = {}
+                if not 'timestamp' in self.data:
+                    self.data['timestamp'] = now
+
+                volt = self.dongle.getObdVoltage()
+                if 'EXTENDED' in self.data and 'auxBatteryVoltage' in self.data['EXTENDED']:
+                    if abs(self.data['EXTENDED']['auxBatteryVoltage'] - volt) > 0.1:
+                        self.dongle.calibrateObdVoltage(self.data['EXTENDED']['auxBatteryVoltage'])
+                        volt = self.dongle.getObdVoltage()
+
+                self.data['ADDITIONAL'].update ({
+                    'obdVoltage':               volt,
+                    'startupThreshold':         thresholds['startup'],
+                    'shutdownThreshold':        thresholds['shutdown'],
+                    'emergencyThreshold':       thresholds['emergency'],
+                    })
+
+        if self.running:
+            runtime = time() - now
+            interval = self.poll_interval - (runtime if runtime > self.poll_interval else 0)
+            self.timer = Timer(interval, self.pollData)
+            self.timer.start()
+
+    def getData(self):
+        with self.datalock.gen_rlock():
+            return self.data if len(self.data) > 0 else None
+
+    def checkWatchdog(self):
+        return (time() - self.watchdog) <= self.watchdog_timeout
--- cars/IONIQ_BEV.py
+++ cars/IONIQ_BEV.py
@@ -1,25 +1,29 @@
 from car import *
 from time import time
 
-b2101 = bytes.fromhex(hex(0x2101)[2:])
-b2105 = bytes.fromhex(hex(0x2105)[2:])
-b2180 = bytes.fromhex(hex(0x2180)[2:])
+b2101 = bytes.fromhex('2101')
+b2102 = bytes.fromhex('2102')
+b2103 = bytes.fromhex('2103')
+b2104 = bytes.fromhex('2104')
+b2105 = bytes.fromhex('2105')
+b2180 = bytes.fromhex('2180')
+b22b002 = bytes.fromhex('22b002')
 
 class IONIQ_BEV(Car):
 
-    def __init__(self, dongle):
-        self.dongle = dongle
+    def __init__(self, config, dongle):
+        Car.__init__(self, config, dongle)
         self.dongle.setProtocol('CAN_11_500')
         self.dongle.setCANRxFilter(0x7ec)
         self.dongle.setCANRxMask(0x7ff)
 
-    def getData(self):
+    def readDongle(self):
         now = time()
         raw = {}
 
         self.dongle.setCANRxFilter(0x7ec)
         self.dongle.setCanID(0x7e4)
-        for cmd in [b2101,b2105]:
+        for cmd in [b2101,b2102,b2103,b2104,b2105]:
             raw[cmd] = self.dongle.sendCommand(cmd)
 
         self.dongle.setCANRxFilter(0x7ee)
@@ -29,55 +33,93 @@ class IONIQ_BEV(Car):
         if len(raw[b2101][0x7ec]) != 9 or \
                 len(raw[b2105][0x7ec]) != 7 or \
                 len(raw[b2180][0x7ee]) != 4:
-            raise IONIQ_BEV.NULL_BLOCK("Got wrong count of frames!\n"+str(raw))
-
-        if raw[b2101][0x7ec][7] == b'\x00\x00\x00\x00\x00\x00\x00':
-            raise IONIQ_BEV.NULL_BLOCK("Got Null Block!\n"+str(raw))
+            raise DataError("Got wrong count of frames!\n"+str(raw))
 
         data = self.getBaseData()
 
+        data['timestamp']   = now
         data['SOC_BMS']     = raw[b2101][0x7ec][1][0] / 2.0
         data['SOC_DISPLAY'] = raw[b2105][0x7ec][4][6] / 2.0
 
         chargingBits = raw[b2101][0x7ec][1][5]
-        dcBatteryCurrent = int.from_bytes(raw[b2101][0x7ec][1][6:7] + raw[b2101][0x7ec][2][0:1], byteorder='big', signed=True) / 10.0
-        dcBatteryVoltage = int.from_bytes(raw[b2101][0x7ec][2][1:3], byteorder='big', signed=False) / 10.0
+        dcBatteryCurrent = ifbs(raw[b2101][0x7ec][1][6:7] + raw[b2101][0x7ec][2][0:1]) / 10.0
+        dcBatteryVoltage = ifbu(raw[b2101][0x7ec][2][1:3]) / 10.0
 
-        # Calc avg battery temp
-        avgBattTemp = (
-                int.from_bytes(raw[b2101][0x7ec][2][5:6], byteorder='big', signed=True) +
-                int.from_bytes(raw[b2101][0x7ec][2][6:7], byteorder='big', signed=True) +
+        cellTemps = [
+                ifbs(raw[b2101][0x7ec][2][5:6]), #  0
+                ifbs(raw[b2101][0x7ec][2][6:7]), #  1
+                ifbs(raw[b2101][0x7ec][3][0:1]), #  2
+                ifbs(raw[b2101][0x7ec][3][1:2]), #  3
+                ifbs(raw[b2101][0x7ec][3][2:3]), #  4
+                ifbs(raw[b2105][0x7ec][1][5:6]), #  5
+                ifbs(raw[b2105][0x7ec][1][6:7]), #  6
+                ifbs(raw[b2105][0x7ec][2][0:1]), #  7
+                ifbs(raw[b2105][0x7ec][2][1:2]), #  8
+                ifbs(raw[b2105][0x7ec][2][2:3]), #  9
+                ifbs(raw[b2105][0x7ec][2][3:4]), # 10
+                ifbs(raw[b2105][0x7ec][2][4:5])] # 11
 
-                int.from_bytes(raw[b2101][0x7ec][3][0:1], byteorder='big', signed=True) +
-                int.from_bytes(raw[b2101][0x7ec][3][1:2], byteorder='big', signed=True) +
-                int.from_bytes(raw[b2101][0x7ec][3][2:3], byteorder='big', signed=True) +
-
-                int.from_bytes(raw[b2105][0x7ec][1][5:6], byteorder='big', signed=True) +
-                int.from_bytes(raw[b2105][0x7ec][1][6:7], byteorder='big', signed=True) +
-
-                int.from_bytes(raw[b2105][0x7ec][2][0:1], byteorder='big', signed=True) +
-                int.from_bytes(raw[b2105][0x7ec][2][1:2], byteorder='big', signed=True) +
-                int.from_bytes(raw[b2105][0x7ec][2][2:3], byteorder='big', signed=True) +
-                int.from_bytes(raw[b2105][0x7ec][2][3:4], byteorder='big', signed=True) +
-                int.from_bytes(raw[b2105][0x7ec][2][4:5], byteorder='big', signed=True) ) / 12
+        cellVoltages = []
+        for cmd in [b2102, b2103, b2104]:
+            for cell in range(0,31):
+                frame = int(cell / 7) + 1
+                byte = int(cell % 7)
+                cellVoltages.append(ifbu(raw[cmd][0x7ec][frame][byte:byte+1]) / 50.0)
 
         data['EXTENDED'] = {
                 'auxBatteryVoltage':        raw[b2101][0x7ec][4][4] / 10.0,
-                'batteryInletTemperature':  int.from_bytes(raw[b2101][0x7ec][3][2:3], byteorder='big', signed=True),
-                'batteryMaxTemperature':    int.from_bytes(raw[b2101][0x7ec][2][3:4], byteorder='big', signed=True),
-                'batteryMinTemperature':    int.from_bytes(raw[b2101][0x7ec][2][4:5], byteorder='big', signed=True),
-                'batteryAvgTemperature':    avgBattTemp,
+
+                'batteryInletTemperature':  ifbs(raw[b2101][0x7ec][3][2:3]),
+                'batteryMaxTemperature':    ifbs(raw[b2101][0x7ec][2][3:4]),
+                'batteryMinTemperature':    ifbs(raw[b2101][0x7ec][2][4:5]),
+
+                'cumulativeEnergyCharged':  ifbu(raw[b2101][0x7ec][5][6:7] + raw[b2101][0x7ec][6][0:3]) / 10.0,
+                'cumulativeEnergyDischarged': ifbu(raw[b2101][0x7ec][6][3:7]) / 10.0,
+
                 'charging':                 1 if chargingBits & 0x80 else 0,
                 'normalChargePort':         1 if chargingBits & 0x20 else 0,
                 'rapidChargePort':          1 if chargingBits & 0x40 else 0,
+
                 'dcBatteryCurrent':         dcBatteryCurrent,
                 'dcBatteryPower':           dcBatteryCurrent * dcBatteryVoltage / 1000.0,
                 'dcBatteryVoltage':         dcBatteryVoltage,
-                'soh':                      int.from_bytes(raw[b2105][0x7ec][4][0:2], byteorder='big', signed=False) / 10.0,
-                'cumulativeEnergyCharged':  int.from_bytes(raw[b2101][0x7ec][5][6:7] + raw[b2101][0x7ec][6][0:3], byteorder='big', signed=False) / 10.0,
-                'cumulativeEnergyDischarged': int.from_bytes(raw[b2101][0x7ec][6][3:7], byteorder='big', signed=False) / 10.0,
-                'externalTemperature':      (raw[b2180][0x7ee][2][1] - 80) / 2,
+
+                'soh':                      ifbu(raw[b2105][0x7ec][4][0:2]) / 10.0,
+                'externalTemperature':      (raw[b2180][0x7ee][2][1] - 80) / 2.0,
                 }
+
+        if data['EXTENDED']['charging'] == 0:
+            self.dongle.setCANRxFilter(0x7ce)
+            self.dongle.setCanID(0x7c6)
+            raw[b22b002] = self.dongle.sendCommand(b22b002)
+            if len(raw[b22b002][0x7ce]) == 3:
+                data['EXTENDED'].update({
+                    'odo':                  ffbu(raw[b22b002][0x7ce][1][3:6]),
+                    })
+
+        data['ADDITIONAL'] = {
+                'cumulativeChargeCurrent':  ifbu(raw[b2101][0x7ec][4][5:7] + raw[b2101][0x7ec][5][0:2]) / 10.0,
+                'cumulativeDischargeCurrent': ifbu(raw[b2101][0x7ec][5][2:6]) / 10.0,
+
+                'batteryAvgTemperature':    sum(cellTemps) / len(cellTemps),
+                'driveMotorSpeed':          ifbs(raw[b2101][0x7ec][8][0:2]),
+
+                'fanStatus':                ifbu(raw[b2101][0x7ec][4][2:3]),
+                'fanFeedback':              ifbu(raw[b2101][0x7ec][4][3:4]),
+
+                'availableChargePower':     ifbu(raw[b2101][0x7ec][1][1:3]) / 100.0,
+                'availableDischargePower':  ifbu(raw[b2101][0x7ec][1][3:5]) / 100.0,
+
+                'obdVoltage':               self.dongle.getObdVoltage(),
+                }
+
+        for i,temp in enumerate(cellTemps):
+            key = "cellTemp{:02d}".format(i+1)
+            data['ADDITIONAL'][key] = float(temp)
+
+        for i,cvolt in enumerate(cellVoltages):
+            key = "cellVoltage{:02d}".format(i+1)
+            data['ADDITIONAL'][key] = float(cvolt)
 
         return data
 
@@ -86,6 +128,10 @@ class IONIQ_BEV(Car):
             "CAPACITY": 28,
             "SLOW_SPEED": 2.3,
             "NORMAL_SPEED": 4.6,
-            "FAST_SPEED": 50
+            "FAST_SPEED": 50.0
         }
+
+    def getABRPModel(self): return 'hyundai:ioniq:17:28:other'
+
+    def getEVNModel(self): return 'IONIQ_BEV'
 
