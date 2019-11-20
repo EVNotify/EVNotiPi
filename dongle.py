9f8af33d285440a65bb9c4137b20c11b7e3ebb4b
Malte Schröder <malte@tnxip.de>
2019-11-20 12:36:23 +0100

    add raw can debug infrastructure

--- dongle.py
+++ dongle.py
@@ -1,3 +1,46 @@
+import requests
+import logging
+
 class CanError(Exception): pass
 class NoData(Exception): pass
 
+class CanDebug:
+    def __init__(self, config, cartype, akey):
+        self.log = logging.getLogger("EVNotiPi/CanDebug")
+        self.session = requests.Session()
+        self.url = config['debug']['url']
+        self.user = config['debug']['user'] if 'user' in config['debug'] else None
+        self.pwd  = config['debug']['pass'] if 'pass' in config['debug'] else None
+        self.cartype = cartype
+        self.akey = akey
+        self.data_queue = []
+
+    def debug(self, cantx, canrx, cmd, data):
+        json = {
+                'cantx': cantx,
+                'canrx': canrx,
+                'command': cmd,
+                'rawdata': data
+                }
+        self.data_queue.append(json)
+        try:
+            self.session.post(self.url, data=self.data_queue, auth=(self.user, self.pwd), timeout=0.1)
+            self.data_queue = []
+        except requests.exceptions.Timeout as e:
+            self.log.info("Timeout occured {}".format(e))
+
+class OBDDongle:
+    def __init__(self, config):
+        if 'debug' in config and config['debug']:
+            self.candebug = CanDebug(config)
+        else:
+            self.candebug = None
+
+    def pushDebug(self, cantx, canrx, cmd, data):
+        if self.candebug:
+            self.candebug.debug(cantx, canrx, cmd, data)
+
+    def sendCommand2(self, cmd):
+        data = self.sendCommand(cmd)
+        # Make data linear here ...
+        return data
