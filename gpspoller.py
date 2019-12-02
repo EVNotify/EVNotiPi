acf08533fbf0366052d4b8c6d43e0b3b1f182900
Malte Schröder <malte@tnxip.de>
2019-12-02 21:06:08 +0100

    remove left over RWLock

--- gpspoller.py
+++ gpspoller.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append('/usr/local/lib/python3.7/site-packages')
 import gps
 import threading
-from readerwriterlock import rwlock
 from time import time,sleep
 from math import isnan
 
@@ -13,7 +12,6 @@ class GpsPoller(threading.Thread):
         self.gpsd = None
         self.last_fix = None
         self.distance = 0
-        self.lock = rwlock.RWLockWrite()
         self.watchdog = time()
         self.watchdog_timeout = 60
 
@@ -42,8 +40,7 @@ class GpsPoller(threading.Thread):
                             last_loop = now
 
                         fix.distance = self.distance
-                        with self.lock.gen_wlock():
-                            self.last_fix = fix
+                        self.last_fix = fix
                 else:
                     self.gpsd = self.gps.gps(mode=self.gps.WATCH_ENABLE)
             except (StopIteration, ConnectionResetError, OSError):
@@ -52,8 +49,7 @@ class GpsPoller(threading.Thread):
                 sleep(1)
 
     def fix(self):
-        with self.lock.gen_rlock():
-            return self.last_fix
+        return self.last_fix
 
     def stop(self):
         self.running = False
