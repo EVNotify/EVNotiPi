e1e5d0fc8677b163e79613ae4b62216c4222075f
Malte Schröder <malte@tnxip.de>
2019-12-08 20:12:39 +0100

    modify test to test the car module

--- ze50test.py
+++ ze50test.py
@@ -3,49 +3,25 @@
 import sys
 import logging
 from binascii import hexlify
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
 
 logging.basicConfig(level=logging.DEBUG)
 
 sys.path.insert(0, 'dongles')
 from SocketCAN import SocketCAN
+sys.path.insert(0, 'cars')
+import ZOE_ZE50
 
 config = {
         'type': 'SocketCAN',
-        'port': 'can0',
-        'speed': 500000
+        'port': 'vcan0',
+        'speed': 500000,
         }
 
 dongle = SocketCAN(config, watchdog=None)
 
-b19023b = bytes.fromhex('19023b')
-b222001 = bytes.fromhex('222001')
-b222002 = bytes.fromhex('222002')
-b222003 = bytes.fromhex('222003')
-b222004 = bytes.fromhex('222004')
-b222005 = bytes.fromhex('222005')
+car = ZOE_ZE50.ZOE_ZE50({'interval': 1}, dongle)
 
-b22500d = bytes.fromhex('22500d')
-b225017 = bytes.fromhex('225017')
-b225026 = bytes.fromhex('225026')
-b225027 = bytes.fromhex('225027')
+pp.pprint(car.readDongle())
 
-b22503a = bytes.fromhex('22503a')
-b22503b = bytes.fromhex('22503b')
-b22503f = bytes.fromhex('22503f')
-
-b225041 = bytes.fromhex('225041')
-b225042 = bytes.fromhex('225042')
-b22504a = bytes.fromhex('22504a')
-
-b225054 = bytes.fromhex('225054')
-b225057 = bytes.fromhex('225057')
-b225058 = bytes.fromhex('225058')
-b225059 = bytes.fromhex('225059')
-b22505a = bytes.fromhex('22505a')
-
-b225062 = bytes.fromhex('225062')
-b225064 = bytes.fromhex('225064')
-
-data = dongle.sendCommandEx(b19023b, canrx=0x18DAF1DE, cantx=0x18DADEF1)
-
-print(hexlify(data))
