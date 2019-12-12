827f20beac2bb6ff4b5f620f79f77771c9f8632b
Malte Schröder <malte@tnxip.de>
2019-12-12 18:35:35 +0100

    fix gps thread

--- gpspoller.py
+++ gpspoller.py
@@ -53,7 +53,7 @@ class GpsPoller:
 
     def stop(self):
         self.running = False
-        self.join()
+        self.thread.join()
 
     def checkWatchdog(self):
         return self.thread.is_alive()
