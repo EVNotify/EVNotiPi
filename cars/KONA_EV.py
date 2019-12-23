4fc0452065b1dbe2af9402979baecd59b62a473e
Malte Schröder <malte@tnxip.de>
2019-12-23 22:13:25 +0100

    Port KONA to new car api

--- cars/KONA_EV.py
+++ cars/KONA_EV.py
@@ -10,46 +10,41 @@ class KONA_EV(Car):
     def __init__(self, config, dongle):
         Car.__init__(self, config, dongle)
         self.dongle.setProtocol('CAN_11_500')
-        self.dongle.setCANRxMask(0x7ff)
 
     def readDongle(self):
         now = time()
         raw = {}
 
-        self.dongle.setCANRxFilter(0x7ec)
-        self.dongle.setCanID(0x7e4)
         for cmd in [b220101,b220105]:
-            raw[cmd] = self.dongle.sendCommand(cmd)
+            raw[cmd] = self.dongle.sendCommandEx(cmd, canrx=0x7ec, cantx=0x7e4)
 
-        self.dongle.setCANRxFilter(0x7ce)
-        self.dongle.setCanID(0x7c6)
         #raw[b220100] = self.dongle.sendCommand(b220100)
-        raw[b22b002] = self.dongle.sendCommand(b22b002)
+        raw[b22b002] = self.dongle.sendCommandEx(b22b002, canrx=0x7ce, cantx=0x7c6)
 
         data = self.getBaseData()
 
         data['timestamp'] = now
-        data['SOC_BMS'] = raw[b220101][0x7ec][1][2] / 2.0
-        data['SOC_DISPLAY'] = raw[b220105][0x7ec][5][0] / 2.0
+        data['SOC_BMS'] = raw[b220101][8] / 2.0
+        data['SOC_DISPLAY'] = raw[b220105][34] / 2.0
 
-        chargingBits = raw[b220101][0x7ec][1][6]
-        dcBatteryCurrent = ifbs(raw[b220101][0x7ec][2][0:2]) / 10.0
-        dcBatteryVoltage = ifbu(raw[b220101][0x7ec][2][2:4]) / 10.0
+        chargingBits = raw[b220101][12]
+        dcBatteryCurrent = ifbs(raw[b220101][13:15]) / 10.0
+        dcBatteryVoltage = ifbu(raw[b220101][15:17]) / 10.0
 
         data['EXTENDED'] = {
-                'auxBatteryVoltage':        raw[b220101][0x7ec][4][5] / 10.0,
-                'batteryInletTemperature':  ifbs(raw[b220101][0x7ec][3][5:6]),
-                'batteryMaxTemperature':    ifbs(raw[b220101][0x7ec][2][4:5]),
-                'batteryMinTemperature':    ifbs(raw[b220101][0x7ec][2][5:6]),
+                'auxBatteryVoltage':        raw[b220101][32] / 10.0,
+                'batteryInletTemperature':  ifbs(raw[b220101][25:26]),
+                'batteryMaxTemperature':    ifbs(raw[b220101][17:18]),
+                'batteryMinTemperature':    ifbs(raw[b220101][18:19]),
                 'charging':                 1 if chargingBits & 0x40 else 0,
                 'normalChargePort':         1 if chargingBits & 0x10 else 0,
                 'rapidChargePort':          1 if chargingBits & 0x20 else 0,
                 'dcBatteryCurrent':         dcBatteryCurrent,
                 'dcBatteryPower':           dcBatteryCurrent * dcBatteryVoltage / 1000.0,
                 'dcBatteryVoltage':         dcBatteryVoltage,
-                'soh':                      ifbu(raw[b220105][0x7ec][4][1:3]) / 10.0,
+                'soh':                      ifbu(raw[b220105][28:30]) / 10.0,
                 #'externalTemperature':      (raw[b220100][0x7ce][1][3] - 80) / 2.0,
-                'odo':                      ffbu(raw[b22b002][0x7ce][1][5:7] + raw[b22b002][0x7ce][2][0:2]),
+                'odo':                      ffbu(raw[b22b002][11:15]),
                 }
 
         return data
