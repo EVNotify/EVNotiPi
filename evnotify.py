06e22c71b4f79559a21e5b340474193f8561e7d1
Malte Schröder <malte@tnxip.de>
2020-01-14 20:37:17 +0100

    allow sleep(0) to guarantee yield

--- evnotify.py
+++ evnotify.py
@@ -191,8 +191,7 @@ class EVNotify:
             if self.running:
                 runtime = time() - now
                 interval = self.poll_interval - (runtime if runtime > self.poll_interval else 0)
-                if interval > 0:
-                    sleep(interval)
+                sleep(min(0, interval))
 
 
     def checkWatchdog(self):
