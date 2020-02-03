be73ead8a27f5e2bc7e35b394f9b8a2c36b25ea3
noradtux <noradtux@users.noreply.github.com>
2020-02-03 15:18:54 +0100

    Fix init handling to avoid blocking if evnotify backend is unreachable (#57)

--- evnotify.py
+++ evnotify.py
@@ -50,14 +50,6 @@ class EVNotify:
         self.settings = None
         self.socThreshold = 100
 
-        self.log.info("Get settings from backend")
-        while self.settings == None:
-            try:
-                self.settings = self.evnotify.getSettings()
-            except evnotifyapi.CommunicationError as e:
-                self.log.info("Waiting for network connectivity")
-                sleep(3)
-
     def start(self):
         self.running = True
         self.thread = Thread(target = self.submitData, name = "EVNotiPi/EVNotify")
@@ -78,6 +70,14 @@ class EVNotify:
             self.data_lock.notify()
 
     def submitData(self):
+        self.log.info("Get settings from backend")
+        while self.settings == None:
+            try:
+                self.settings = self.evnotify.getSettings()
+            except evnotifyapi.CommunicationError as e:
+                self.log.info("Waiting for network connectivity")
+                sleep(3)
+
         while self.running:
             with self.data_lock:
                 self.log.debug('Waiting...')
@@ -96,7 +96,7 @@ class EVNotify:
                         'longitude': [],
                         'altitude': [],
                         }
-               
+
                 for d in self.data:
                     for k,v in avgs.items():
                         if k in d and d[k] != None:
