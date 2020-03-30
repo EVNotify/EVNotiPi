4936b67adc3169e1db6a32390ba948208eee5026
Malte Schröder <malte@tnxip.de>
2020-03-30 17:24:54 +0200

    properly use importlib instead of exec-hack

--- car/__init__.py
+++ car/__init__.py
@@ -1,9 +1,8 @@
 import os
+from importlib import import_module
 
 def Load(car_type):
     if not "{}.py".format(car_type) in os.listdir('car'):
         raise Exception('Unsupported car {}'.format(car_type))
 
-    exec("from car.{0} import {0} as CAR".format(car_type))
-
-    return CAR
+    return getattr(import_module("car." + car_type), car_type)
--- dongle/__init__.py
+++ dongle/__init__.py
@@ -1,9 +1,8 @@
 import os
+from importlib import import_module
 
 def Load(dongle_type):
     if not "{}.py".format(dongle_type) in os.listdir('dongle'):
         raise Exception('Unsupported dongle {}'.format(dongle_type))
 
-    exec("from dongle.{0} import {0} as DONGLE".format(dongle_type))
-
-    return DONGLE
+    return getattr(import_module("dongle." + dongle_type), dongle_type)
