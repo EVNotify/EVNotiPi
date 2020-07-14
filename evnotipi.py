98c7bfca0cf5cdbae0544ce85c7292a05c07d4ab
Malte Schröder <malte.schroeder@tnxip.de>
2020-07-14 21:12:29 +0200

    PEP8 cleanup. Removed unused code.

--- evnotipi.py
+++ evnotipi.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
+""" EVNotiPi main module """
 
 from gevent.monkey import patch_all; patch_all()
-from gpspoller import GpsPoller
-from subprocess import check_call, check_output
-from time import sleep,time
-import os
-import sys
-import signal
-import sdnotify
 import logging
+import os
+import signal
+import sys
 from argparse import ArgumentParser
+from subprocess import check_call, check_output
+from time import sleep, time
+import sdnotify
+from gpspoller import GpsPoller
 import evnotify
 import car
 import dongle
@@ -17,12 +18,16 @@ import watchdog
 
 Systemd = sdnotify.SystemdNotifier()
 
+
 class ThreadFailure(Exception):
     """ Raised when a sub thread fails """
 
+
 parser = ArgumentParser(description='EVNotiPi')
-parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False)
-parser.add_argument('-c', '--config', dest='config', action='store', default='config.yaml')
+parser.add_argument('-d', '--debug', dest='debug',
+                    action='store_true', default=False)
+parser.add_argument('-c', '--config', dest='config',
+                    action='store', default='config.yaml')
 args = parser.parse_args()
 del parser
 
@@ -55,13 +60,13 @@ log = logging.getLogger("EVNotiPi")
 del args
 
 # emulate old config if watchdog section is missing
-if not 'watchdog' in config or not 'type' in config['watchdog']:
+if 'watchdog' not in config or 'type' not in config['watchdog']:
     log.warning('Old watchdog config syntax detected. Please adjust according to config.yaml.template.')
     config['watchdog'] = {
         'type': 'GPIO',
         'shutdown_pin': config['dongle'].get('shutdown_pin', 24),
         'pup_down': config['dongle'].get('pup_down', 21),
-        }
+    }
 
 # Load OBD2 interface module
 DONGLE = dongle.load(config['dongle']['type'])
@@ -92,19 +97,12 @@ Threads.append(car)
 EVNotify = evnotify.EVNotify(config['evnotify'], car)
 Threads.append(EVNotify)
 
-# Init WiFi control
-if 'wifi' in config and config['wifi'].get('enable') == True:
-    from wifi_ctrl import WiFiCtrl
-    wifi = WiFiCtrl()
-else:
-    wifi = None
-
-# Init some variables
-main_running = True
 
 # Set up signal handling
 def exit_gracefully(signum, frame):
+    """ Signalhandler for SIGTERM """
     sys.exit(0)
+
 
 signal.signal(signal.SIGTERM, exit_gracefully)
 
@@ -112,53 +110,48 @@ signal.signal(signal.SIGTERM, exit_gracefully)
 for t in Threads:
     t.start()
 
-Systemd.notify("READY=1")
-log.info("Starting main loop")
+Systemd.notify('READY=1')
+log.info('Starting main loop')
+
+main_running = True
 try:
     while main_running:
         now = time()
         threads_ok = True
         for t in Threads:
             status = t.check_thread()
-            if status == False:
-                log.error("Thread Failed (%s)", str(t))
+            if not status:
+                log.error('Thread Failed (%s)', str(t))
                 threads_ok = False
                 raise ThreadFailure(str(t))
 
         if threads_ok:
-            Systemd.notify("WATCHDOG=1")
+            Systemd.notify('WATCHDOG=1')
 
         if 'system' in config and 'shutdown_delay' in config['system']:
             if (now - car.last_data > config['system']['shutdown_delay'] and
-                    watchdog.isCarAvailable() == False):
-                usercnt = int(check_output(['who','-q']).split(b'\n')[1].split(b'=')[1])
+                    not watchdog.isCarAvailable()):
+                usercnt = int(check_output(['who', '-q']).split(b'\n')[1].split(b'=')[1])
                 if usercnt == 0:
-                    log.info("Not charging and car off => Shutdown")
-                    check_call(['/bin/systemctl','poweroff'])
+                    log.info('Not charging and car off => Shutdown')
+                    check_call(['/bin/systemctl', 'poweroff'])
                     sleep(5)
                 else:
-                    log.info("Not charging and car off; Not shutting down, users connected")
+                    log.info('Not charging and car off; Not shutting down, users connected')
 
-        if wifi and config['wifi']['shutdown_delay'] != None:
-            if (now - car.last_data > config['wifi']['shutdown_delay'] and
-                    watchdog.isCarAvailable() == False):
-                wifi.disable()
-            else:
-                wifi.enable()
-
+        # Ensure messages get printed to the console.
         sys.stdout.flush()
 
         if main_running:
             loop_delay = 1 - (time()-now)
-            if loop_delay > 0: sleep(loop_delay)
+            sleep(max(0, loop_delay))
 
-except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+except (KeyboardInterrupt, SystemExit):  # when you press ctrl+c
     main_running = False
-    Systemd.notify("STOPPING=1")
+    Systemd.notify('STOPPING=1')
 finally:
-    Systemd.notify("STOPPING=1")
-    log.info("Exiting ...")
-    for t in Threads[::-1]: # reverse Threads
+    Systemd.notify('STOPPING=1')
+    log.info('Exiting ...')
+    for t in Threads[::-1]:  # reverse Threads
         t.stop()
-    log.info("Bye.")
-
+    log.info('Bye.')
