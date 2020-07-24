07fd193bb3dc9537720f7a057840b747a032a871
Malte Schröder <malte.schroeder@tnxip.de>
2020-07-24 20:15:00 +0200

    Convert all ISO-TP cars to the generic ISO-TP decoder.

--- car/ioniq_fl_ev.py
+++ car/ioniq_fl_ev.py
@@ -4,3 +4,17 @@ from .kona_ev import KonaEv
 
 class IoniqFlEv(KonaEv):
     """ Class for Hyundai Ioniq Electric 38kWh """
+
+    def get_base_data(self):
+        return {
+            "CAPACITY": 38,
+            "SLOW_SPEED": 2.3,
+            "NORMAL_SPEED": 4.6,
+            "FAST_SPEED": 50.0
+        }
+
+    def get_abrp_model(self):
+        return 'hyundai:ioniq:19:38:other'
+
+    def get_evn_model(self):
+        return 'IONIQ_FL_EV'
--- car/kona_ev.py
+++ car/kona_ev.py
@@ -1,60 +1,101 @@
 """ Module for the Hyundai Kona EV """
-from .car import Car, ifbs, ifbu, ffbu
-from dongle import NoData
+from .car import Car
+from .isotp_decoder import IsoTpDecoder
 
 b220100 = bytes.fromhex('220100')
 b220101 = bytes.fromhex('220101')
+b220102 = bytes.fromhex('220102')
+b220103 = bytes.fromhex('220103')
+b220104 = bytes.fromhex('220104')
 b220105 = bytes.fromhex('220105')
 b22b002 = bytes.fromhex('22b002')
 
+Fields = (
+    {'cmd': b220101, 'canrx': 0x7ec, 'cantx': 0x7e4,
+     'fields': (
+         {'padding': 7},
+         {'name': 'SOC_BMS', 'width': 1, 'scale': .5},
+         {'padding': 4},
+         {'name': 'charging_bits1', 'width': 1},
+         {'name': 'dcBatteryCurrent', 'width': 2, 'signed': True, 'scale': .1},
+         {'name': 'dcBatteryVoltage', 'width': 2, 'scale': .1},
+         {'name': 'batteryMaxTemperature', 'width': 1, 'signed': True},
+         {'name': 'batteryMinTemperature', 'width': 1, 'signed': True},
+         {'name': 'cellTemp%02d', 'idx': 1, 'cnt': 4, 'width': 1, 'signed': True},
+         {'padding': 2},
+         {'name': 'batteryInletTemperature', 'width': 1, 'signed': True},
+         {'padding': 6},
+         {'name': 'auxBatteryVoltage', 'width': 1, 'scale': .1},
+         {'name': 'cumulativeChargeCurrent', 'width': 4, 'scale': .1},
+         {'name': 'cumulativeDischargeCurrent', 'width': 4, 'scale': .1},
+         {'name': 'cumulativeEnergyCharged', 'width': 4, 'scale': .1},
+         {'name': 'cumulativeEnergyDischarged', 'width': 4, 'scale': .1},
+         {'name': 'operatingTime', 'width': 4},  # seconds
+         {'name': 'charging_bits2', 'width': 1},
+         {'padding': 8},
+     )
+     },
+    {'cmd': b220102, 'canrx': 0x7ec, 'cantx': 0x7e4,
+     'fields': (
+         {'padding': 7},
+         {'name': 'cellVoltage%02d', 'idx': 1, 'cnt': 32, 'width': 1, 'scale': .02},
+     )
+     },
+    {'cmd': b220103, 'canrx': 0x7ec, 'cantx': 0x7e4,
+     'fields': (
+         {'padding': 7},
+         {'name': 'cellVoltage%02d', 'idx': 33, 'cnt': 32, 'width': 1, 'scale': .02},
+     )
+     },
+    {'cmd': b220104, 'canrx': 0x7ec, 'cantx': 0x7e4,
+     'fields': (
+         {'padding': 7},
+         {'name': 'cellVoltage%02d', 'idx': 65, 'cnt': 32, 'width': 1, 'scale': .02},
+     )
+     },
+    {'cmd': b220105, 'canrx': 0x7ec, 'cantx': 0x7e4,
+     'fields': (
+         {'padding': 28},
+         {'name': 'soh', 'width': 2, 'scale': .1},
+         {'padding': 4},
+         {'name': 'SOC_DISPLAY', 'width': 1, 'scale': .5},
+         {'padding': 11},
+     )
+     },
+    {'cmd': b22b002, 'canrx': 0x7ce, 'cantx': 0x7c6, 'optional': True,
+     'fields': (
+         {'padding': 9},
+         {'name': 'odo', 'width': 3},
+         {'padding': 3},
+     )
+     },
+    {'computed': True,
+     'fields': (
+         {'name': 'dcBatteryPower',
+          'lambda': lambda d: d['dcBatteryCurrent'] * d['dcBatteryVoltage'] / 1000.0},
+         {'name': 'charging',
+          'lambda': lambda d: int(d['charging_bits2'] & 0xc == 0x8)},
+         {'name': 'normalChargePort',
+          'lambda': lambda d: int((d['charging_bits2'] & 0x80) != 0 and d['charging_bits1'] == 3)},
+         {'name': 'rapidChargePort',
+          'lambda': lambda d: int((d['charging_bits2'] & 0x80) != 0 and d['charging_bits1'] != 3)},
+     )
+     },
+)
+
 
 class KonaEv(Car):
-    """ Class for Hyundai Kona EV """
+    """ Decoder class for Hyundai Kona EV """
 
     def __init__(self, config, dongle, watchdog, gps):
         Car.__init__(self, config, dongle, watchdog, gps)
         self._dongle.set_protocol('CAN_11_500')
+        self._isotp = IsoTpDecoder(self.dongle, Fields)
 
     def read_dongle(self, data):
         """ Read and parse data from dongle """
-        raw = {}
-
-        for cmd in [b220101, b220105]:
-            raw[cmd] = self._dongle.send_command_ex(cmd, canrx=0x7ec, cantx=0x7e4)
-
-        try:
-            raw[b22b002] = self._dongle.send_command_ex(b22b002, canrx=0x7ce, cantx=0x7c6)
-        except NoData:
-            # 0x7ce is only available while driving
-            pass
-
-        data.update(self.get_base_data())
-
-        charging_bits = raw[b220101][53]
-        dc_battery_current = ifbs(raw[b220101][13:15]) / 10.0
-        dc_battery_voltage = ifbu(raw[b220101][15:17]) / 10.0
-
-        data.update({
-            # Base:
-            'SOC_BMS':                  raw[b220101][7] / 2.0,
-            'SOC_DISPLAY':              raw[b220105][34] / 2.0,
-            # Extended:
-            'auxBatteryVoltage':        raw[b220101][32] / 10.0,
-            'batteryInletTemperature':  ifbs(raw[b220101][25:26]),
-            'batteryMaxTemperature':    ifbs(raw[b220101][17:18]),
-            'batteryMinTemperature':    ifbs(raw[b220101][18:19]),
-            'cumulativeEnergyCharged':  ifbu(raw[b220101][41:45]) / 10.0,
-            'cumulativeEnergyDischarged': ifbu(raw[b220101][45:49]) / 10.0,
-            'charging':                 1 if (charging_bits & 0xc) == 0x8 else 0,
-            'normalChargePort':         1 if (charging_bits & 0x80) and raw[b220101][12] == 3 else 0,
-            'rapidChargePort':          1 if (charging_bits & 0x80) and raw[b220101][12] != 3 else 0,
-            'dcBatteryCurrent':         dc_battery_current,
-            'dcBatteryPower':           dc_battery_current * dc_battery_voltage / 1000.0,
-            'dcBatteryVoltage':         dc_battery_voltage,
-            'soh':                      ifbu(raw[b220105][28:30]) / 10.0,
-            # 'externalTemperature':      (raw[b220100][0x7ce][1][3] - 80) / 2.0,
-            'odo':                      ffbu(raw[b22b002][11:15]) if b22b002 in raw else None,
-        })
+        data.update(self.getBaseData())
+        data.update(self._isotp.get_data())
 
     def get_base_data(self):
         return {
@@ -63,3 +104,6 @@ class KonaEv(Car):
             "NORMAL_SPEED": 4.6,
             "FAST_SPEED": 50.0
         }
+
+    def get_evn_model(self):
+        return 'KONA_EV'
