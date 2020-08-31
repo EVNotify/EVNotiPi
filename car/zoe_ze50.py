58934fd751b610e03a1e7050c3799fcdd9d4baea
Malte Schröder <malte.schroeder@tnxip.de>
2020-08-31 07:50:35 +0200

    Fixup missing linefeeds and minor stuff

--- car/zoe_ze50.py
+++ car/zoe_ze50.py
@@ -130,7 +130,6 @@ class ZoeZe50(Car):
         data.update(self.get_base_data())
         data.update(self._isotp.get_data())
 
-
     def get_base_data(self):
         return {
             "CAPACITY": 50,
